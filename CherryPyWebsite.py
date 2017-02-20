e714b6c658671d1a6b7b4c3371ec7e60989213ce
wardt7 <wardt7@uni.coventry.ac.uk>
2017-02-20 11:37:02 +0000

    - CSS is now being served via CherryPy.

--- CherryPyWebsite.py
+++ CherryPyWebsite.py
@@ -6,5 +6,24 @@ class home():
         return open("index.html")
 
 if __name__ == "__main__":
-	cherrypy.tree.mount(home(), "/", "website.conf")
-	cherrypy.engine.start()
+    current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
+    config = {
+    'global': {
+        'environment': 'production',
+        'log.screen': True,
+        'server.socket_host': '127.0.0.1',
+        'server.socket_port': 8080,
+        'engine.autoreload_on': True,
+        'log.error_file': os.path.join(current_dir, 'errors.log'),
+        'log.access_file': os.path.join(current_dir, 'access.log'),
+    },
+    '/':{
+        'tools.staticdir.root' : current_dir,
+    },
+    '/static':{
+        'tools.staticdir.on' : True,
+        'tools.staticdir.dir' : 'static',
+    },
+}
+cherrypy.quickstart(home(), '/', config)
+
