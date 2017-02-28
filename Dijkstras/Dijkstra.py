4428ae4d56c6b91e26b66f63580ee50c3d0acb0a
James Lee <jamesl33info@gmail.com>
2017-02-28 08:57:43 +0000

    added functions to get closest connection and get all node info

--- Dijkstras/Dijkstra.py
+++ Dijkstras/Dijkstra.py
@@ -16,7 +16,19 @@ class Dijkstra():
         cur.execute(("INSERT INTO Nodes VALUES (?, ?)"), (node, pdata))
         self.conn.commit()
 
-    def unpickleFetch(self, node):
+    def unPickleFetch(self, node):
+        try:
+            nodeInfo = []
+            cur = self.conn.cursor()
+            cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
+            currentNode = cur.fetchone()
+            nodeInfo.append(currentNode[0])
+            nodeInfo.append(pickle.loads(currentNode[1]))
+            print(nodeInfo)
+        except TypeError:
+            print("That node does not exist!")
+
+    def fetchConnections(self, node):
         try:
             cur = self.conn.cursor()
             cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
@@ -25,13 +37,26 @@ class Dijkstra():
         except TypeError:
             print("That node does not exist!")
 
+    def getClosestNode(self, node):
+        try:
+            cur = self.conn.cursor()
+            cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
+            currentNode = cur.fetchone()
+            currentNode = pickle.loads(currentNode[1])
+            print(min(currentNode, key=currentNode.get))
+        except TypeError:
+            print("That node does not exist!")
 
 if __name__ == "__main__":
     shortPath = Dijkstra()
     try:
         shortPath.conn = sql.connect("Nodes.sqlite3")
-        #shortPath.pickleStore("ECG-15", [1,2,3])
-        #shortPath.unpickleFetch("ECG-15")
+        # shortPath.pickleStore("ECG-13", {"ECG-14": 5, "ECG-15": 3})
+        # shortPath.pickleStore("ECG-14", {"ECG-13": 3, "ECG-15": 6})
+        # shortPath.pickleStore("ECG-15", {"ECG-13": 3, "ECG-14": 6})
+        # shortPath.pickleStore("ECG-27", {"Main Entrance": 1})
+        # shortPath.pickleStore("First Floor Stairs", ["ECG-15"])
+        # shortPath.unPickleFetch("ECG-15")
     finally:
         shortPath.conn.close()
 
