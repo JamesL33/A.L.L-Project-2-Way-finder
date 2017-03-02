6b7ffe6664682103e35dcab830fc20e170aa070f
edwardbillin22 <billin22@uni.coventry.ac.uk>
2017-03-02 01:20:48 +0000

    Now the site get's your geo-location and shows you the way to the ECC building

--- External Wayfinding/website.py
+++
@@ -1,9 +0,0 @@
-import cherrypy, os, os.path
-
-class host():
-    @cherrypy.expose
-    def index(self):
-        return open("index2.html")
-
-if __name__ == "__main__":
-    cherrypy.quickstart(host())
