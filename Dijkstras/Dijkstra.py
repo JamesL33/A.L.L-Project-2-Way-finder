8ce18ad2f59f0f61505c1843c11881bfe908c2a0
James Lee <jamesl33info@gmail.com>
2017-03-02 15:27:41 +0000

    slight formatting changes and added return statements

--- Dijkstras/Dijkstra.py
+++ Dijkstras/Dijkstra.py
@@ -1,49 +1,51 @@
 import sqlite3 as sql
 import pickle
 
-class Dijkstra():
+
+class Dijkstra:
     def __init__(self):
-        self.__shortestPath = []
+        self.__usedNodes = []
         self.__currentNode = 0
         self.__nodes = {}
 
-    def shortestPath(self):
-        pass
+    def shortest_path(self, node):
+        self.__currentNode = node
+        self.__usedNodes = []
 
-    def pickleStore(self, node, connections):
+    def pickle_store(self, node, connections):
         cur = self.conn.cursor()
         pdata = pickle.dumps(connections, pickle.HIGHEST_PROTOCOL)
         cur.execute(("INSERT INTO Nodes VALUES (?, ?)"), (node, pdata))
         self.conn.commit()
 
-    def unPickleFetch(self, node):
+    def pickle_fetch(self, node):
         try:
             nodeInfo = []
-            cur = self.conn.cursor()
-            cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
-            currentNode = cur.fetchone()
+            self.cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
+            currentNode = self.cur.fetchone()
             nodeInfo.append(currentNode[0])
             nodeInfo.append(pickle.loads(currentNode[1]))
-            print(nodeInfo)
+            return(nodeInfo)
         except TypeError:
             print("That node does not exist!")
 
-    def fetchConnections(self, node):
+    def fetch_connections(self, node):
         try:
-            cur = self.conn.cursor()
-            cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
-            currentNode = cur.fetchone()
-            print(pickle.loads((currentNode[1])))
+            self.cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
+            currentNode = self.cur.fetchone()
+            return(pickle.loads((currentNode[1])))
         except TypeError:
             print("That node does not exist!")
 
-    def getClosestNode(self, node):
+    def get_closest_node(self, node):
         try:
-            cur = self.conn.cursor()
-            cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
-            currentNode = cur.fetchone()
+            closest = []
+            self.cur.execute(("Select * FROM Nodes WHERE Name = (?)"), (node,))
+            currentNode = self.cur.fetchone()
             currentNode = pickle.loads(currentNode[1])
-            print(min(currentNode, key=currentNode.get))
+            closest.append((min(currentNode, key=currentNode.get)))
+            closest.append(currentNode[closest[0]])
+            return closest
         except TypeError:
             print("That node does not exist!")
 
@@ -51,12 +53,13 @@ if __name__ == "__main__":
     shortPath = Dijkstra()
     try:
         shortPath.conn = sql.connect("Nodes.sqlite3")
+        shortPath.cur = shortPath.conn.cursor()
         # shortPath.pickleStore("ECG-13", {"ECG-14": 5, "ECG-15": 3})
         # shortPath.pickleStore("ECG-14", {"ECG-13": 3, "ECG-15": 6})
         # shortPath.pickleStore("ECG-15", {"ECG-13": 3, "ECG-14": 6})
         # shortPath.pickleStore("ECG-27", {"Main Entrance": 1})
         # shortPath.pickleStore("First Floor Stairs", ["ECG-15"])
-        # shortPath.unPickleFetch("ECG-15")
+        print(shortPath.get_closest_node("ECG-15"))
     finally:
         shortPath.conn.close()
 
