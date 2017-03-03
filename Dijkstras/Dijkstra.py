0f8b615ef4bb20b7269045a4dc3effcd8d363ba5
James Lee <jamesl33info@gmail.com>
2017-03-03 11:23:15 +0000

    Added method to return graph of nodes from database. Started short_path method. Minor changes.

--- Dijkstras/Dijkstra.py
+++ Dijkstras/Dijkstra.py
@@ -3,14 +3,19 @@ import pickle
 
 
 class Dijkstra:
-    def __init__(self):
-        self.__usedNodes = []
-        self.__currentNode = 0
-        self.__nodes = {}
+    def __init__(self, db):
+        self.conn = sql.connect(db)
+        self.cur = self.conn.cursor()
+        self.__progress = {}
 
-    def shortest_path(self, node):
-        self.__currentNode = node
-        self.__usedNodes = []
+    def short_path(self, graph, start, end):
+        # progress = {node: [previously visited nodes], distance}
+        self.__progress[start] = [["ECG-15"], 0]
+        del graph[start]
+        for key in graph:
+            self.__progress[key] = [[], float("inf")]
+
+        print(self.__progress)
 
     def pickle_store(self, node, connections):
         cur = self.conn.cursor()
@@ -49,17 +54,28 @@ class Dijkstra:
         except TypeError:
             print("That node does not exist!")
 
+    def return_graph(self):
+        graph = {}
+        try:
+            self.cur.execute("Select * FROM Nodes")
+            while True:
+                currentNode = self.cur.fetchone()
+                if currentNode == None:
+                    break
+                graph[currentNode[0]] = pickle.loads(currentNode[1])
+            return graph
+        except TypeError:
+            print("There has been an error")
+
 if __name__ == "__main__":
-    shortPath = Dijkstra()
+    shortPath = Dijkstra("Nodes.sqlite3")
     try:
-        shortPath.conn = sql.connect("Nodes.sqlite3")
-        shortPath.cur = shortPath.conn.cursor()
         # shortPath.pickleStore("ECG-13", {"ECG-14": 5, "ECG-15": 3})
         # shortPath.pickleStore("ECG-14", {"ECG-13": 3, "ECG-15": 6})
         # shortPath.pickleStore("ECG-15", {"ECG-13": 3, "ECG-14": 6})
         # shortPath.pickleStore("ECG-27", {"Main Entrance": 1})
         # shortPath.pickleStore("First Floor Stairs", ["ECG-15"])
-        print(shortPath.get_closest_node("ECG-15"))
+        print(shortPath.short_path(shortPath.return_graph(), "ECG-15", "First Floor Stairs"))
     finally:
         shortPath.conn.close()
 
