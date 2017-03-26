4bc701895281eeb9e07ced80c893cd0116bd68e2
Ed <billin22@uni.coventry.ac.uk>
2017-03-26 19:03:06 +0100

    Markers are now done

--- static/js/markers.js
+++ static/js/markers.js
@@ -20,31 +20,6 @@ function createMarker(lat, long, map, info)
 	});
 }
 
-var beep = [
-"Alan Berry",
-"Alma",
-"Armstrong Siddeley",
-"Bugatti",
-"Charles Ward",
-"EEC",
-"Ellen Terry",
-"George Eliot",
-"Graham Sutherland",
-"Jaguar",
-"James Starley",
-"Library",
-"Maurice Foss",
-"Multi Storey Car Park",
-"Priory",
-"Richard Crossman",
-"Sir John Laing",
-"Sir William Lyons",
-"Sports and Recreation Centre",
-"Student Centre",
-"The Hub",
-"Whitefriars",
-"William Morris"];
-
 var locations = {
 	"52.408141 -1.506584" : "Alan Berry",
 	"52.410193 -1.500576" : "Alma",
