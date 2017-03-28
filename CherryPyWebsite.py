1fde6e27a8e9b34ba21749765935b9f8c3ac7f56
wardt7 <wardt7@uni.coventry.ac.uk>
2017-03-28 18:59:22 +0100

    - Added logoff page - Changed indentation to use 4 spaces instead of tabs because Codio hates tabs for some reason

--- CherryPyWebsite.py
+++ CherryPyWebsite.py
@@ -8,105 +8,109 @@ from dominate.tags import *
 
 
 class home():
+    @cherrypy.expose
+    def index(self):
+        return open("index.html")
 
-	@cherrypy.expose
-	def index(self):
-		return open("index.html")
+    @cherrypy.expose
+    def campusNav(self):
+        return open("campusNav.html")
 
-	@cherrypy.expose
-	def campusNav(self):
-		return open("campusNav.html")
+    @cherrypy.expose
+    def covUniBuildings(self):
+        return open("covUniBuildings.html")
 
-	@cherrypy.expose
-	def covUniBuildings(self):
-		return open("covUniBuildings.html")
+    @cherrypy.expose
+    def ecBuildingNav(self):
+        return open("ecBuildingNav.html")
 
-	@cherrypy.expose
-	def ecBuildingNav(self):
-		return open("ecBuildingNav.html")
+    @cherrypy.expose
+    def about(self):
+        return open("about.html")
 
-	@cherrypy.expose
-	def about(self):
-		return open("about.html")
+    @cherrypy.expose
+    def campusMap(self):
+        return open("campusMap.html")
 
-	@cherrypy.expose
-	def campusMap(self):
-		return open("campusMap.html")
+    @cherrypy.expose
+    def admin(self, username=None, userpassword=None):
+        if cherrypy.session.get('loggedon'):
+            return open("logoff.html")
+        if username is None or userpassword is None:
+            return open("admin.html")
+        with security.Security() as password_check:
+            try:
+                password_check.log_on(username, userpassword)
+                # replace with successful page
+                cherrypy.session["loggedon"] = True
+                return open("logoff.html")
+            except ValueError:
+                # replace with unsuccessful page
+                return open("adminFailLogin.html")
 
-	@cherrypy.expose
-	def admin(self, username=None, userpassword=None):
-		if cherrypy.session.get('loggedon'):
-			return open("about.html")
-		if username is None or userpassword is None:
-			return open("admin.html")
-		with security.Security() as password_check:
-			try:
-				password_check.log_on(username, userpassword)
-				# replace with successful page
-				cherrypy.session["loggedon"] = True
-				return open("about.html")
-			except ValueError:
-				# replace with unsuccessful page
-				return open("adminFailLogin.html")
+    @cherrypy.expose
+    def ecBuildingNav(self, start=None, end=None):
+        if start is None or end is None:
+            return open("ecBuildingNav.html")
+        elif start is "" or end is "":
+            return open("ecBuildingNav.html")
+        else:
+            try:
+                edges = Pathfinding.Database.get_edges(Pathfinding.Database())
+                vertices = Pathfinding.Database.get_vertices(Pathfinding.Database())
+                graph = Pathfinding.Graph()
+                for edge in edges:
+                    graph.add_edge(edge[0], edge[1], edge[2])
+                for vertex in vertices:
+                    graph.add_vertex(vertex)
 
-	@cherrypy.expose
-	def ecBuildingNav(self, start=None, end=None):
-		if start is None or end is None:
-			return open("ecBuildingNav.html")
-		elif start is "" or end is "":
-			return open("ecBuildingNav.html")
-		else:
-			try:
-				edges = Pathfinding.Database.get_edges(Pathfinding.Database())
-				vertices = Pathfinding.Database.get_vertices(Pathfinding.Database())
-				graph = Pathfinding.Graph()
-				for edge in edges:
-					graph.add_edge(edge[0], edge[1], edge[2])
-				for vertex in vertices:
-					graph.add_vertex(vertex)
+                path, distance = Pathfinding.short_path(graph, start, end)
 
-				path, distance = Pathfinding.short_path(graph, start, end)
+                doc = dominate.document(title='Indoor Navigation')
+                with doc.head:
+                    link(rel='stylesheet', type='text/css',
+                         href='static/css/ecBuildingNav.css')
 
-				doc = dominate.document(title='Indoor Navigation')
-				with doc.head:
-					link(rel='stylesheet', type='text/css',
-						 href='static/css/ecBuildingNav.css')
+                with doc:
+                    with div(id='header').add(ol()):
+                        for i in path:
+                            li(a(i.title()))
 
-				with doc:
-					with div(id='header').add(ol()):
-						for i in path:
-							li(a(i.title()))
+                    with div():
+                        attr(cls='body')
+                        p("The cost of this journey is {0}".format(distance))
 
-					with div():
-						attr(cls='body')
-						p("The cost of this journey is {0}".format(distance))
+                return str(doc)
 
-				return str(doc)
+            except TypeError as e:
+                print("Your starting point or destination is not in the graph")
+                print(e)
 
-			except TypeError as e:
-				print("Your starting point or destination is not in the graph")
-				print(e)
+    def logoff(self):
+        cherrypy.session["loggedon"] = False
+        return self.index()
+
 
 if __name__ == "__main__":
-	current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
-	config = {
-		'global': {
-			'environment': 'production',
-			'log.screen': True,
-			'server.socket_host': '127.0.0.1',
-			'server.socket_port': 8080,
-			'engine.autoreload_on': True,
-			'log.error_file': os.path.join(current_dir, 'errors.log'),
-			'log.access_file': os.path.join(current_dir, 'access.log'),
-		},
-		'/': {
-			'tools.staticdir.root': current_dir,
-			'tools.sessions.on': True,
-			'tools.sessions.timeout': 15,
-		},
-		'/static': {
-			'tools.staticdir.on': True,
-			'tools.staticdir.dir': 'static',
-		}
-	}
+    current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
+    config = {
+        'global': {
+            'environment': 'production',
+            'log.screen': True,
+            'server.socket_host': '127.0.0.1',
+            'server.socket_port': 8080,
+            'engine.autoreload_on': True,
+            'log.error_file': os.path.join(current_dir, 'errors.log'),
+            'log.access_file': os.path.join(current_dir, 'access.log'),
+        },
+        '/': {
+            'tools.staticdir.root': current_dir,
+            'tools.sessions.on': True,
+            'tools.sessions.timeout': 15,
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': 'static',
+        }
+    }
 cherrypy.quickstart(home(), '/', config)
--- Pathfinding.py
+++ Pathfinding.py
@@ -5,8 +5,7 @@ import sqlite3 as sql
 
 
 class Graph:
-
-	''' DataType to represent a graph. inspired by https://gist.github.com/econchick/4666413
+    ''' DataType to represent a graph. inspired by https://gist.github.com/econchick/4666413
 
 	Class Attributes:
 			self.vertices
@@ -24,29 +23,28 @@ class Graph:
 
 	'''
 
-	def __init__(self):
-		self.vertices = set()
-		self.edges = collections.defaultdict(list)
-		self.weights = {}
+    def __init__(self):
+        self.vertices = set()
+        self.edges = collections.defaultdict(list)
+        self.weights = {}
 
-	def add_vertex(self, value):
-		''' Add a new vertex to the graph '''
-		self.vertices.add(value)
+    def add_vertex(self, value):
+        ''' Add a new vertex to the graph '''
+        self.vertices.add(value)
 
-	def add_edge(self, from_vertex, to_vertex, distance):
-		''' Adds a connection between two vertices by adding an entry to edges with
+    def add_edge(self, from_vertex, to_vertex, distance):
+        ''' Adds a connection between two vertices by adding an entry to edges with
 		the from and to node '''
-		if from_vertex == to_vertex:
-			pass
-		self.edges[from_vertex].append(to_vertex)
-		self.edges[to_vertex].append(from_vertex)
-		self.weights[(from_vertex, to_vertex)] = distance
-		self.weights[(to_vertex, from_vertex)] = distance
+        if from_vertex == to_vertex:
+            pass
+        self.edges[from_vertex].append(to_vertex)
+        self.edges[to_vertex].append(from_vertex)
+        self.weights[(from_vertex, to_vertex)] = distance
+        self.weights[(to_vertex, from_vertex)] = distance
 
 
 class Database:
-
-	''' Database class which handles the storing and collection of data from "ECC_Building.sqlite3"
+    ''' Database class which handles the storing and collection of data from "ECC_Building.sqlite3"
 
 	I have used a database here instead of just creating the Graph on the fly. This does not save time when
 	the code is run however in the long run this means that this code can be reused by just linking a different
@@ -63,59 +61,59 @@ class Database:
 
 	'''
 
-	def __init__(self):
-		self.connection = sql.connect('ECC_Building.sqlite3')
-		self.cursor = self.connection.cursor()
+    def __init__(self):
+        self.connection = sql.connect('ECC_Building.sqlite3')
+        self.cursor = self.connection.cursor()
 
-	def add_vertex(self, vertex):
-		''' Adds 'vertex' to the 'vertices' table in the database '''
+    def add_vertex(self, vertex):
+        ''' Adds 'vertex' to the 'vertices' table in the database '''
 
-		self.cursor.execute(('INSERT INTO Vertices VALUES (?)'), (vertex,))
-		self.connection.commit()
+        self.cursor.execute(('INSERT INTO Vertices VALUES (?)'), (vertex,))
+        self.connection.commit()
 
-	def add_edge(self, edge):
-		''' Adds 'edge' to the 'Edges' table in the database '''
+    def add_edge(self, edge):
+        ''' Adds 'edge' to the 'Edges' table in the database '''
 
-		pdata = pickle.dumps(edge, pickle.HIGHEST_PROTOCOL)
-		self.cursor.execute(('INSERT INTO Edges VALUES (?)'), (pdata,))
-		self.connection.commit()
+        pdata = pickle.dumps(edge, pickle.HIGHEST_PROTOCOL)
+        self.cursor.execute(('INSERT INTO Edges VALUES (?)'), (pdata,))
+        self.connection.commit()
 
-	def get_edges(self):
-		''' Gets all the edges from the 'Edges' table in ECC_Building.sqlite3 and returns them in a list '''
+    def get_edges(self):
+        ''' Gets all the edges from the 'Edges' table in ECC_Building.sqlite3 and returns them in a list '''
 
-		edges = []
+        edges = []
 
-		self.cursor.execute('SELECT * FROM Edges')
+        self.cursor.execute('SELECT * FROM Edges')
 
-		while True:
-			pdata = self.cursor.fetchone()
-			if not pdata:
-				break
-			else:
-				edge = pickle.loads(pdata[0])
-				edges.append(edge)
+        while True:
+            pdata = self.cursor.fetchone()
+            if not pdata:
+                break
+            else:
+                edge = pickle.loads(pdata[0])
+                edges.append(edge)
 
-		return edges
+        return edges
 
-	def get_vertices(self):
-		''' Gets all the vertices from the 'vertices' table in ECC_Building.sqlite3 and returns them in a list '''
+    def get_vertices(self):
+        ''' Gets all the vertices from the 'vertices' table in ECC_Building.sqlite3 and returns them in a list '''
 
-		vertices = []
+        vertices = []
 
-		self.cursor.execute('SELECT * FROM Vertices')
+        self.cursor.execute('SELECT * FROM Vertices')
 
-		while True:
-			vertex = self.cursor.fetchone()
-			if not vertex:
-				break
-			else:
-				vertices.append(vertex[0])
+        while True:
+            vertex = self.cursor.fetchone()
+            if not vertex:
+                break
+            else:
+                vertices.append(vertex[0])
 
-		return vertices
+        return vertices
 
 
 def dijkstra(graph, start_node):
-	'''
+    '''
 	Method variables:
 					visited
 					Type: set
@@ -134,95 +132,95 @@ def dijkstra(graph, start_node):
 
 	'''
 
-	# initialize variables
-	visited = set()
+    # initialize variables
+    visited = set()
 
-	# progress represents the distance from start to vertex. It is created with each distance
-	# being equal to 'math.inf' this is because we do not know the distance to
-	# any node yet.
-	progress = dict.fromkeys(list(graph.vertices), math.inf)
-	predecessors = dict.fromkeys(list(graph.vertices), None)
+    # progress represents the distance from start to vertex. It is created with each distance
+    # being equal to 'math.inf' this is because we do not know the distance to
+    # any node yet.
+    progress = dict.fromkeys(list(graph.vertices), math.inf)
+    predecessors = dict.fromkeys(list(graph.vertices), None)
 
-	# Update the start node in progress to the value 0.
-	progress[start_node] = 0
+    # Update the start node in progress to the value 0.
+    progress[start_node] = 0
 
-	# While there is a node that is not in visited
-	while visited != graph.vertices:
-		# vertex becomes the closest node that has not yet been visited. It
-		# will begin at 'start_node'
-		vertex = min((set(progress.keys()) - visited), key=progress.get)
+    # While there is a node that is not in visited
+    while visited != graph.vertices:
+        # vertex becomes the closest node that has not yet been visited. It
+        # will begin at 'start_node'
+        vertex = min((set(progress.keys()) - visited), key=progress.get)
 
-		# for each node which is not in visited
-		for neighborNode in set(graph.edges[vertex]) - visited:
-			# New path is set to the current distance value in 'progress[vertex]
-			# plus the new weight distance'
-			testPath = progress[vertex] + graph.weights[vertex, neighborNode]
+        # for each node which is not in visited
+        for neighborNode in set(graph.edges[vertex]) - visited:
+            # New path is set to the current distance value in 'progress[vertex]
+            # plus the new weight distance'
+            testPath = progress[vertex] + graph.weights[vertex, neighborNode]
 
-			# If testPath is shorted than current path 'progress[neighborNode]'
-			if testPath < progress[neighborNode]:
-				# Update path to shorted 'testPath'
-				progress[neighborNode] = testPath
+            # If testPath is shorted than current path 'progress[neighborNode]'
+            if testPath < progress[neighborNode]:
+                # Update path to shorted 'testPath'
+                progress[neighborNode] = testPath
 
-				# Update previous vertex of the neighborNode
-				predecessors[neighborNode] = vertex
-		visited.add(vertex)
-	return (progress, predecessors)
+                # Update previous vertex of the neighborNode
+                predecessors[neighborNode] = vertex
+        visited.add(vertex)
+    return (progress, predecessors)
 
 
 def short_path(graph, start, end):
-	''' Uses the Dijkstra Method to return the shortest path from 'start' to 'end' '''
+    ''' Uses the Dijkstra Method to return the shortest path from 'start' to 'end' '''
 
-	if start not in graph.vertices or end not in graph.vertices:
-		raise TypeError("Your starting point or destination is not in the graph")
+    if start not in graph.vertices or end not in graph.vertices:
+        raise TypeError("Your starting point or destination is not in the graph")
 
-	# Update progress and predecessors with the Dijkstra method
-	progress, predecessors = dijkstra(graph, start)
+    # Update progress and predecessors with the Dijkstra method
+    progress, predecessors = dijkstra(graph, start)
 
-	# initialize variables
-	currentPath = []
-	vertex = end
+    # initialize variables
+    currentPath = []
+    vertex = end
 
-	while vertex != None:
-		currentPath.append(vertex)
-		vertex = predecessors[vertex]
+    while vertex != None:
+        currentPath.append(vertex)
+        vertex = predecessors[vertex]
 
-	return currentPath[::-1], progress[end]
+    return currentPath[::-1], progress[end]
 
 # if __name__ == '__main__':
 
-	############################## First Unit Testing graph ##################
+############################## First Unit Testing graph ##################
 
-	# Graph = Graph()
-	# # Vertices's
-	# Graph.add_vertex('a')
-	# Graph.add_vertex('b')
-	# Graph.add_vertex('c')
-	# Graph.add_vertex('d')
-	# Graph.add_vertex('e')
-	# # Edges
-	# Graph.add_edge('a', 'b', 5)
-	# Graph.add_edge('a', 'c', 6)
-	# Graph.add_edge('a', 'd', 10)
-	# Graph.add_edge('b', 'd', 6)
-	# Graph.add_edge('c', 'd', 6)
-	# Graph.add_edge('d', 'e', 2)
+# Graph = Graph()
+# # Vertices's
+# Graph.add_vertex('a')
+# Graph.add_vertex('b')
+# Graph.add_vertex('c')
+# Graph.add_vertex('d')
+# Graph.add_vertex('e')
+# # Edges
+# Graph.add_edge('a', 'b', 5)
+# Graph.add_edge('a', 'c', 6)
+# Graph.add_edge('a', 'd', 10)
+# Graph.add_edge('b', 'd', 6)
+# Graph.add_edge('c', 'd', 6)
+# Graph.add_edge('d', 'e', 2)
 
-	############################## Second Unit Testing graph #################
+############################## Second Unit Testing graph #################
 
-	# db = Database()
-	# Graph = Graph()
+# db = Database()
+# Graph = Graph()
 
-	# edges = db.get_edges() # get edges
-	# vertices = db.get_vertices() # get vertices's
+# edges = db.get_edges() # get edges
+# vertices = db.get_vertices() # get vertices's
 
-	# for edge in edges:
-	# 	Graph.add_edge(edge[0], edge[1], edge[2])
-	# for vertex in vertices:
-	# 	Graph.add_vertex(vertex)
+# for edge in edges:
+# 	Graph.add_edge(edge[0], edge[1], edge[2])
+# for vertex in vertices:
+# 	Graph.add_vertex(vertex)
 
-	#Unit testing loop to test all variations of
+# Unit testing loop to test all variations of
 
-	#Unit Test for Dijkstra's Algorithm
-	# for node in set(Graph.vertices):
-	# 	for vertex in set(Graph.vertices):
-	# 		print('From: {0}, To: {1}'.format(node, vertex), (short_path(Graph, node, vertex)))
+# Unit Test for Dijkstra's Algorithm
+# for node in set(Graph.vertices):
+# 	for vertex in set(Graph.vertices):
+# 		print('From: {0}, To: {1}'.format(node, vertex), (short_path(Graph, node, vertex)))
