83a32219bd9986da55e2ae9d86b02b02820145e3
James Lee <jamesl33info@gmail.com>
2017-03-29 16:37:25 +0100

    Moved small amount of code and added distance traved to indoor nav

--- CherryPyWebsite.py
+++ CherryPyWebsite.py
@@ -8,109 +8,108 @@ from dominate.tags import *
 
 
 class home():
-    @cherrypy.expose
-    def index(self):
-        return open("index.html")
+	@cherrypy.expose
+	def index(self):
+		return open("index.html")
 
-    @cherrypy.expose
-    def campusNav(self):
-        return open("campusNav.html")
+	@cherrypy.expose
+	def campusNav(self):
+		return open("campusNav.html")
 
-    @cherrypy.expose
-    def covUniBuildings(self):
-        return open("covUniBuildings.html")
+	@cherrypy.expose
+	def covUniBuildings(self):
+		return open("covUniBuildings.html")
 
-    @cherrypy.expose
-    def ecBuildingNav(self):
-        return open("ecBuildingNav.html")
+	@cherrypy.expose
+	def ecBuildingNav(self):
+		return open("ecBuildingNav.html")
 
-    @cherrypy.expose
-    def about(self):
-        return open("about.html")
+	@cherrypy.expose
+	def about(self):
+		return open("about.html")
 
-    @cherrypy.expose
-    def campusMap(self):
-        return open("campusMap.html")
+	@cherrypy.expose
+	def campusMap(self):
+		return open("campusMap.html")
 
-    @cherrypy.expose
-    def admin(self, username=None, userpassword=None):
-        if cherrypy.session.get('loggedon'):
-            return open("logoff.html")
-        if username is None or userpassword is None:
-            return open("admin.html")
-        with security.Security() as password_check:
-            try:
-                password_check.log_on(username, userpassword)
-                # replace with successful page
-                cherrypy.session["loggedon"] = True
-                return open("logoff.html")
-            except ValueError:
-                # replace with unsuccessful page
-                return open("adminFailLogin.html")
+	@cherrypy.expose
+	def admin(self, username=None, userpassword=None):
+		if cherrypy.session.get('loggedon'):
+			return open("logoff.html")
+		if username is None or userpassword is None:
+			return open("admin.html")
+		with security.Security() as password_check:
+			try:
+				password_check.log_on(username, userpassword)
+				# replace with successful page
+				cherrypy.session["loggedon"] = True
+				return open("logoff.html")
+			except ValueError:
+				# replace with unsuccessful page
+				return open("adminFailLogin.html")
 
-    @cherrypy.expose
-    def ecBuildingNav(self, start=None, end=None):
-        if start is None or end is None:
-            return open("ecBuildingNav.html")
-        elif start is "" or end is "":
-            return open("ecBuildingNav.html")
-        else:
-            try:
-                edges = Pathfinding.Database.get_edges(Pathfinding.Database())
-                vertices = Pathfinding.Database.get_vertices(Pathfinding.Database())
-                graph = Pathfinding.Graph()
-                for edge in edges:
-                    graph.add_edge(edge[0], edge[1], edge[2])
-                for vertex in vertices:
-                    graph.add_vertex(vertex)
+	def logoff(self):
+		cherrypy.session["loggedon"] = False
+		return self.index()
 
-                path, distance = Pathfinding.short_path(graph, start, end)
+	@cherrypy.expose
+	def ecBuildingNav(self, start=None, end=None):
+		if start is None or end is None:
+			return open("ecBuildingNav.html")
+		elif start is "" or end is "":
+			return open("ecBuildingNav.html")
+		else:
+			try:
+				edges = Pathfinding.Database.get_edges(Pathfinding.Database())
+				vertices = Pathfinding.Database.get_vertices(Pathfinding.Database())
+				graph = Pathfinding.Graph()
+				for edge in edges:
+					graph.add_edge(edge[0], edge[1], edge[2])
+				for vertex in vertices:
+					graph.add_vertex(vertex)
 
-                doc = dominate.document(title='Indoor Navigation')
-                with doc.head:
-                    link(rel='stylesheet', type='text/css',
-                         href='static/css/ecBuildingNav.css')
+				path, distance = Pathfinding.short_path(graph, start, end)
 
-                with doc:
-                    with div(id='header').add(ol()):
-                        for i in path:
-                            li(a(i.title()))
+				doc = dominate.document(title='Indoor Navigation')
+				with doc.head:
+					link(rel='stylesheet', type='text/css',
+						 href='static/css/ecBuildingNav.css')
 
-                    with div():
-                        attr(cls='body')
-                        p("The cost of this journey is {0}".format(distance))
+				with doc:
+					with div(id='header').add(ol()):
+						for i in path:
+							li(a(i.title()))
 
-                return str(doc)
+					with div():
+						attr(cls='body')
+						p("The cost of this journey is {0} meters".format(distance))
 
-            except TypeError as e:
-                print("Your starting point or destination is not in the graph")
-                print(e)
+				return str(doc)
 
-    def logoff(self):
-        cherrypy.session["loggedon"] = False
-        return self.index()
-
+			except TypeError as e:
+				print("Your starting point or destination is not in the graph")
+				print(e)
 
 if __name__ == "__main__":
-    current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
-    config = {
-        'global': {
-            'environment': 'production',
-            'log.screen': True,
-            'server.socket_host': '127.0.0.1',
-            'server.socket_port': 8080,
-            'engine.autoreload_on': True,
-            'log.error_file': os.path.join(current_dir, 'errors.log'),
-            'log.access_file': os.path.join(current_dir, 'access.log'),
-        },
-        '/': {
-            'tools.staticdir.root': current_dir,
-            'tools.sessions.on': True,
-            'tools.sessions.timeout': 15,
-        },
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': 'static',
-        }
-    }
+	current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
+	config = {
+		'global': {
+			'environment': 'production',
+			'log.screen': True,
+			'server.socket_host': '127.0.0.1',
+			'server.socket_port': 8080,
+			'engine.autoreload_on': True,
+			'log.error_file': os.path.join(current_dir, 'errors.log'),
+			'log.access_file': os.path.join(current_dir, 'access.log'),
+		},
+		'/': {
+			'tools.staticdir.root': current_dir,
+			'tools.sessions.on': True,
+			'tools.sessions.timeout': 15,
+		},
+		'/static': {
+			'tools.staticdir.on': True,
+			'tools.staticdir.dir': 'static',
+		}
+	}
 cherrypy.quickstart(home(), '/', config)
